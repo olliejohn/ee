ddbcedf4807c6928f6f81f66083734dada47e834
Ollie Etherington <ollie.etherington@gmail.com>
2014-04-28 20:12:49 +0100

    Formatted vterm_read.c and vterm_write.c as well as some (very) small refactoring

--- src/vte/vterm_read.c
+++ src/vte/vterm_read.c
@@ -22,13 +22,12 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <poll.h>
 #include <errno.h>
 #include <limits.h>
-
+#include <poll.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -43,87 +42,85 @@
 
 ssize_t vterm_read_pipe(vterm_t *vterm)
 {
-   struct pollfd     fd_array;
-   char 	     *buf=NULL;
-   char              *pos;
-   int               bytes_peek=0;
-   size_t	     bytes_waiting;
-   ssize_t	     bytes_read=0;
-   size_t            bytes_remaining=0;
-   size_t            bytes_total=0;
-   int               retval;
-   pid_t             child_pid;
-   int               pid_status;
-   int               errcpy=0;
+	struct pollfd fd_array;
+	char *buf = NULL, *pos;
+	pid_t child_pid;
+	int retval, pid_status, bytes_peek = 0, errcpy = 0;
+	ssize_t bytes_read = 0;
+	size_t bytes_waiting, bytes_remaining = 0, bytes_total = 0;
 
-   // make sure pty file descriptor is good
-   if(vterm->pty_fd < 0) return -1;
+	/* Make sure pty file descriptor is good */
+	if (vterm->pty_fd < 0)
+		return -1;
 
-   // check to see if child pid has exited
-   child_pid=waitpid(vterm->child_pid,&pid_status,WNOHANG);
+	/* Check to see if child pid has exited */
+	child_pid = waitpid(vterm->child_pid, &pid_status, WNOHANG);
 
-   if(child_pid==vterm->child_pid || child_pid==-1)
-   {
-      vterm->state |= STATE_CHILD_EXITED;
-      return -1;
-   }
+	if (child_pid == vterm->child_pid || child_pid == -1) {
+		vterm->state |= STATE_CHILD_EXITED;
+		return -1;
+	}
 
-   fd_array.fd=vterm->pty_fd;
-   fd_array.events=POLLIN;
+	fd_array.fd = vterm->pty_fd;
+	fd_array.events = POLLIN;
 
-   // wait 10 millisecond for data on pty file descriptor.
-   retval=poll(&fd_array,1,10);
+	/* Wait 10 millisecond for data on pty file descriptor */
+	retval = poll(&fd_array, 1, 10);
 
-   // no data or poll() error.
-   if(retval <= 0)
-   {
-      if(errno == EINTR) return 0;
-      return retval;
-   }
+	/* No data or poll() error */
+	if (retval <= 0)
+		return (errno == EINTR) ? 0 : retval;
 
 #ifdef FIONREAD
-	retval=ioctl(vterm->pty_fd,FIONREAD,&bytes_peek);
+	retval = ioctl(vterm->pty_fd, FIONREAD, &bytes_peek);
 #else
-	retval=ioclt(vterm->pty_fd,TIOCINQ,&bytes_peek);
+	retval = ioclt(vterm->pty_fd, TIOCINQ, &bytes_peek);
 #endif
 
-   if(retval == -1) return 0;
-	if(bytes_peek == 0) return 0;
+	if (retval == -1)
+		return 0;
 
-   bytes_waiting=bytes_peek;
-   if(bytes_waiting > SSIZE_MAX) bytes_waiting=SSIZE_MAX;
-   bytes_remaining=bytes_waiting;
+	if (bytes_peek == 0)
+		return 0;
 
-	buf=(char*)calloc(bytes_waiting+10,sizeof(char));	/* 10 byte padding	*/
-   pos=buf;
+	bytes_waiting = bytes_peek;
 
-   do
-   {
-      bytes_read=read(vterm->pty_fd,pos,bytes_remaining);
-      if(bytes_read == -1)
-      {
-         if(errno == EINTR) bytes_read=0;
-         else errcpy=errno;
-      }
+	if (bytes_waiting > SSIZE_MAX)
+		bytes_waiting = SSIZE_MAX;
 
-      if(bytes_read <= 0) break;
+	bytes_remaining = bytes_waiting;
 
-      bytes_remaining-=bytes_read;
-      bytes_total+=bytes_read;
-      pos+=bytes_read;
-   }
-   while(bytes_remaining > 0);
+	buf = calloc(bytes_waiting + 10, sizeof(char)); /* 10 byte padding */
+	pos = buf;
 
-   // render the data to the offscreen terminal buffer.
-   if((bytes_waiting > 0) && (bytes_read != -1))
-   {
-      vterm_render(vterm,buf,bytes_read);
-   }
+	do {
+		bytes_read = read(vterm->pty_fd, pos, bytes_remaining);
 
-   // release memory
-   if(buf!=NULL) free(buf);
+		if (bytes_read == -1) {
+			if (errno == EINTR)
+				bytes_read = 0;
+			else
+				errcpy = errno;
+		}
 
-   if(bytes_read == -1 && errcpy != EINTR) return -1;
+		if (bytes_read <= 0)
+			break;
 
-   return bytes_waiting-bytes_remaining;
+		bytes_remaining -= bytes_read;
+		bytes_total += bytes_read;
+		pos += bytes_read;
+	} while (bytes_remaining > 0);
+
+	/* Render the data to the offscreen terminal buffer */
+	if ((bytes_waiting > 0) && (bytes_read != -1))
+		vterm_render(vterm, buf, bytes_read);
+
+	/* Release memory */
+	if (buf != NULL)
+		free(buf);
+
+	if (bytes_read == -1 && errcpy != EINTR)
+		return -1;
+
+	return bytes_waiting - bytes_remaining;
 }
--- src/vte/vterm_write.c
+++ src/vte/vterm_write.c
@@ -22,100 +22,91 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-#include <unistd.h>
-#include <string.h>
-
 #include <curses.h>
+#include <string.h>
+#include <unistd.h>
 
 #include "vterm.h"
 #include "vterm_private.h"
 #include "vterm_write.h"
 
-void vterm_write_pipe(vterm_t *vterm,unsigned int keycode)
+void vterm_write_pipe(vterm_t *vterm, unsigned int keycode)
 {
-   if(vterm == NULL) return;
-
-   vterm->write(vterm,keycode);
-
-   return;
+	if (vterm == NULL)
+		return;
+	vterm->write(vterm, keycode);
 }
 
 void vterm_write_rxvt(vterm_t *vterm, unsigned int keycode)
 {
-   char *buffer=NULL;
+	char *buffer = NULL;
 
-   switch(keycode)
-   {
-      case '\n':           buffer="\r";      break;
-      case KEY_UP:         buffer="\e[A";    break;
-      case KEY_DOWN:       buffer="\e[B";    break;
-      case KEY_RIGHT:      buffer="\e[C";    break;
-      case KEY_LEFT:       buffer="\e[D";    break;
-      case KEY_BACKSPACE:  buffer="\b";      break;
-      case KEY_IC:         buffer="\e[2~";   break;
-      case KEY_DC:         buffer="\e[3~";   break;
-      case KEY_HOME:       buffer="\e[7~";   break;
-      case KEY_END:        buffer="\e[8~";   break;
-      case KEY_PPAGE:      buffer="\e[5~";   break;
-      case KEY_NPAGE:      buffer="\e[6~";   break;
-      case KEY_SUSPEND:    buffer="\x1A";    break;      // ctrl-z
-      case KEY_F(1):       buffer="\e[11~";  break;
-      case KEY_F(2):       buffer="\e[12~";  break;
-      case KEY_F(3):       buffer="\e[13~";  break;
-      case KEY_F(4):       buffer="\e[14~";  break;
-      case KEY_F(5):       buffer="\e[15~";  break;
-      case KEY_F(6):       buffer="\e[17~";  break;
-      case KEY_F(7):       buffer="\e[18~";  break;
-      case KEY_F(8):       buffer="\e[19~";  break;
-      case KEY_F(9):       buffer="\e[20~";  break;
-      case KEY_F(10):      buffer="\e[21~";  break;
-      case KEY_F(11):      buffer="\e[23~";  break;
-      case KEY_F(12):      buffer="\e[24~";  break;
-   }
+	switch (keycode) {
+		case '\n':           buffer = "\r";      break;
+		case KEY_UP:         buffer = "\e[A";    break;
+		case KEY_DOWN:       buffer = "\e[B";    break;
+		case KEY_RIGHT:      buffer = "\e[C";    break;
+		case KEY_LEFT:       buffer = "\e[D";    break;
+		case KEY_BACKSPACE:  buffer = "\b";      break;
+		case KEY_IC:         buffer = "\e[2~";   break;
+		case KEY_DC:         buffer = "\e[3~";   break;
+		case KEY_HOME:       buffer = "\e[7~";   break;
+		case KEY_END:        buffer = "\e[8~";   break;
+		case KEY_PPAGE:      buffer = "\e[5~";   break;
+		case KEY_NPAGE:      buffer = "\e[6~";   break;
+		case KEY_SUSPEND:    buffer = "\x1A";    break;  // ctrl-z
+		case KEY_F(1):       buffer = "\e[11~";  break;
+		case KEY_F(2):       buffer = "\e[12~";  break;
+		case KEY_F(3):       buffer = "\e[13~";  break;
+		case KEY_F(4):       buffer = "\e[14~";  break;
+		case KEY_F(5):       buffer = "\e[15~";  break;
+		case KEY_F(6):       buffer = "\e[17~";  break;
+		case KEY_F(7):       buffer = "\e[18~";  break;
+		case KEY_F(8):       buffer = "\e[19~";  break;
+		case KEY_F(9):       buffer = "\e[20~";  break;
+		case KEY_F(10):      buffer = "\e[21~";  break;
+		case KEY_F(11):      buffer = "\e[23~";  break;
+		case KEY_F(12):      buffer = "\e[24~";  break;
+	}
 
-   if(buffer==NULL)
-      write(vterm->pty_fd,&keycode,sizeof(char));
-   else
-      write(vterm->pty_fd,buffer,strlen(buffer));
-
-   return;
+	if (buffer == NULL)
+		write(vterm->pty_fd, &keycode, sizeof(char));
+	else
+		write(vterm->pty_fd, buffer, strlen(buffer));
 }
 
-void vterm_write_vt100(vterm_t *vterm,unsigned int keycode)
+void vterm_write_vt100(vterm_t *vterm, unsigned int keycode)
 {
-   char *buffer=NULL;
+	char *buffer = NULL;
 
-   switch(keycode)
-   {
-      case '\n':           buffer="\r";      break;
-      case KEY_UP:         buffer="\e[A";    break;
-      case KEY_DOWN:       buffer="\e[B";    break;
-      case KEY_RIGHT:      buffer="\e[C";    break;
-      case KEY_LEFT:       buffer="\e[D";    break;
-      case KEY_BACKSPACE:  buffer="\b";      break;
-      case KEY_IC:         buffer="\e[2~";   break;
-      case KEY_DC:         buffer="\e[3~";   break;
-      case KEY_HOME:       buffer="\e[7~";   break;
-      case KEY_END:        buffer="\e[8~";   break;
-      case KEY_PPAGE:      buffer="\e[5~";   break;
-      case KEY_NPAGE:      buffer="\e[6~";   break;
-      case KEY_SUSPEND:    buffer="\x1A";    break;      // ctrl-z
-      case KEY_F(1):       buffer="\e[[A";   break;
-      case KEY_F(2):       buffer="\e[[B";   break;
-      case KEY_F(3):       buffer="\e[[C";   break;
-      case KEY_F(4):       buffer="\e[[D";   break;
-      case KEY_F(5):       buffer="\e[[E";   break;
-      case KEY_F(6):       buffer="\e[17~";  break;
-      case KEY_F(7):       buffer="\e[18~";  break;
-      case KEY_F(8):       buffer="\e[19~";  break;
-      case KEY_F(9):       buffer="\e[20~";  break;
-      case KEY_F(10):      buffer="\e[21~";  break;
-   }
+	switch (keycode) {
+		case '\n':           buffer = "\r";      break;
+		case KEY_UP:         buffer = "\e[A";    break;
+		case KEY_DOWN:       buffer = "\e[B";    break;
+		case KEY_RIGHT:      buffer = "\e[C";    break;
+		case KEY_LEFT:       buffer = "\e[D";    break;
+		case KEY_BACKSPACE:  buffer = "\b";      break;
+		case KEY_IC:         buffer = "\e[2~";   break;
+		case KEY_DC:         buffer = "\e[3~";   break;
+		case KEY_HOME:       buffer = "\e[7~";   break;
+		case KEY_END:        buffer = "\e[8~";   break;
+		case KEY_PPAGE:      buffer = "\e[5~";   break;
+		case KEY_NPAGE:      buffer = "\e[6~";   break;
+		case KEY_SUSPEND:    buffer = "\x1A";    break;  // ctrl-z
+		case KEY_F(1):       buffer = "\e[[A";   break;
+		case KEY_F(2):       buffer = "\e[[B";   break;
+		case KEY_F(3):       buffer = "\e[[C";   break;
+		case KEY_F(4):       buffer = "\e[[D";   break;
+		case KEY_F(5):       buffer = "\e[[E";   break;
+		case KEY_F(6):       buffer = "\e[17~";  break;
+		case KEY_F(7):       buffer = "\e[18~";  break;
+		case KEY_F(8):       buffer = "\e[19~";  break;
+		case KEY_F(9):       buffer = "\e[20~";  break;
+		case KEY_F(10):      buffer = "\e[21~";  break;
+	}
 
-   if(buffer==NULL)
-      write(vterm->pty_fd,&keycode,sizeof(char));
-   else
-      write(vterm->pty_fd,buffer,strlen(buffer));
-
-   return;
+	if (buffer == NULL)
+		write(vterm->pty_fd, &keycode, sizeof(char));
+	else
+		write(vterm->pty_fd, buffer, strlen(buffer));
 }
